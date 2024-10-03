ca473d2dc91bb66291d4170a358c98ff416884f9
Ishtiaque Ahmed CSEH Rafin <94455536+CsehRafin@users.noreply.github.com>
2024-10-03 21:58:01 +0600

    Update script.bat

--- script.bat
+++ script.bat
@@ -3,16 +3,13 @@
 echo Hello, This is a script for "Goofy Ahh SMP", MADE BY CSEHRAFIN.
 mkdir temporary-files
 cd temporary-files
-curl -o better-end-4.30.1.jar https://cdn.modrinth.com/data/gc8OEnCC/versions/WAIDCJK8/better-end-4.30.1.jar 
-curl -o bclib-3.30.1.jar https://cdn.modrinth.com/data/BgNRHReB/versions/HP1eF3Su/bclib-3.30.1.jar 
-curl -o better-nether-9.30.0.jar https://cdn.modrinth.com/data/MpzVLzy5/versions/GcAIYp4g/better-nether-9.30.0.jar 
-curl -o deeperdarker-fabric-1.20.4-1.3.2.1.jar https://cdn.modrinth.com/data/fnAffV0n/versions/Meg92c7E/deeperdarker-fabric-1.20.4-1.3.2.1.jar 
-curl -o owo-lib-0.12.6+1.20.3.jar https://cdn.modrinth.com/data/ccKDOlHs/versions/Her7Z3CG/owo-lib-0.12.6%2B1.20.3.jar 
-curl -o fabricapi-1.20.4.latest.jar https://cdn.modrinth.com/data/P7dR8mSH/versions/QVBohPm2/fabric-api-0.97.2%2B1.20.4.jar
-move fabricapi-1.20.4.latest.jar %appdata%\.minecraft\mods\
-move better-end-4.30.1.jar %appdata%\.minecraft\mods\
-move bclib-3.30.1.jar %appdata%\.minecraft\mods\
-move better-nether-9.30.0.jar %appdata%\.minecraft\mods\
-move deeperdarker-fabric-1.20.4-1.3.2.1.jar %appdata%\.minecraft\mods\
-move owo-lib-0.12.6+1.20.3.jar %appdata%\.minecraft\mods\
-
+curl -o embeddium-0.3.31+1.20.1.jar https://cdn.modrinth.com/data/sk9rgfiA/versions/UTbfe5d1/embeddium-0.3.31%2Bmc1.20.1.jar
+curl -o embeddiumplus-1.20.1-v1.2.13.jar https://cdn.modrinth.com/data/yD9qW65f/versions/KEd4eJfk/embeddiumplus-1.20.1-v1.2.13.jar
+curl -o starlight-1.1.2.jar https://cdn.modrinth.com/data/iRfIGC1s/versions/cNa0vkNj/starlight-1.1.2%2Bforge.1cda73c.jar
+curl -o entityculling-forge-1.7.0-mc1.20.1.jar https://cdn.modrinth.com/data/NNAgCjsB/versions/dlg7nMa6/entityculling-forge-1.7.0-mc1.20.1.jar
+curl -o modernfix-forge-5.19.4+1.20.1.jar https://cdn.modrinth.com/data/nmDcB62a/versions/m83ZRQdk/modernfix-forge-5.19.4%2Bmc1.20.1.jar
+move embeddium-0.3.31+1.20.1.jar %appdata%\.minecraft\mods\
+move embeddiumplus-1.20.1-v1.2.13.jar %appdata%\.minecraft\mods\
+move starlight-1.1.2.jar %appdata%\.minecraft\mods\
+move entityculling-forge-1.7.0-mc1.20.1.jar %appdata%\.minecraft\mods\
+move modernfix-forge-5.19.4+1.20.1.jar %appdata%\.minecraft\mods\
